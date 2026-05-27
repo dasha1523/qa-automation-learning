37c03c13743069fee567a884fcfbba50b0243af2
d.gudkova <M6EJkQAEs/>
2026-05-27 22:15:26 +0300

    Причесала код, добавила обработку ошибки

--- json_parser.py
+++ json_parser.py
@@ -1,20 +1,39 @@
 import json
 
-with open("data.json.py", "r", encoding="utf-8") as b:
-    data_from_json = json.load(b)
+#Указываем путь к файлу с данными
+file_name = "datа.json"
 
+#Читаем файл и переводим в словарь Python + выводим возможную ошибку
+try:
+    with open(file_name, "r", encoding="utf-8") as file:
+        data_from_json = json.load(file)
+except FileNotFoundError:
+    print(f"Файл '{file_name}' не найден")
+    exit()
+
+#Ищем пользователя с id>1
+print("Пользователи с id > 1")
 for user in data_from_json["users"]:
     if user["id"] > 1:
         print(user)
 
-new_user = {"id": 3, "name": "Charlie", "email": "charlie@example.com"}
+#Добавляем нового пользователя
+print("\nДобавляем пользователя Chaelie")
+new_user = {
+    "id": 3,
+    "name": "Charlie",
+    "email": "charlie@example.com"
+}
 data_from_json["users"].append(new_user)
 
-with open("data.json.py", "w", encoding="utf-8") as f:
-    json.dump(data_from_json, f, ensure_ascii=False, indent=4)
+#Сохраняем изменения в файл
+with open(file_name, "w", encoding="utf-8") as file:
+    json.dump(data_from_json, file, ensure_ascii=False, indent=4)
 
-with open("data.json.py", "r", encoding="utf-8") as b:
-    data_from_json = json.load(b)
+#Снова читаем файл и выводим всех пользователей
+with open(file_name, "r", encoding="utf-8") as file:
+    data_from_json = json.load(file)
 
+print("\nСписок всех пользователей и информация о них")
 for key, value in data_from_json.items():
     print(key, value)
