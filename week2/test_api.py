9dcae2ea4e36bb0f214140b951a277ec601f8eb6
d.gudkova <M6EJkQAEs/>
2026-06-16 21:10:40 +0300

    Еще тестики

--- week2/test_api.py
+++ week2/test_api.py
@@ -1,8 +1,17 @@
 import requests
 import pytest
 
-def_test_get_post():
+def test_get_post():
     url = "https://jsonplaceholder.typicode.com/posts/1"
     response = requests.get(url)
 
     assert response.status_code == 200, f" Ожидали 200, получили {response.status_code}"
+    assert "application/json" in response.headers['Content-Type'], "Неверный 'Content-Type'"
+    assert "charset=utf-8" in response.headers['Content-Type'], "Неверный 'Content-Type'"
+
+    data = response.json()
+    assert "title" in data, "В ответе отсутствует поле 'title'"
+    assert isinstance(data["title"], str) in data, "Поле 'title' другого типа"
+
+    assert "userId" in data, "В ответе отсутствует поле 'id'"
+    assert isinstance(data["userId"], int) in data, "Поле 'userId' другого типа"
